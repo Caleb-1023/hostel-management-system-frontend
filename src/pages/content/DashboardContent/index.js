151137d9555991a0adf191dea456e3fcc07baf54
Ese Okpongete <eseokpongs13@gmail.com>
2022-08-10 11:56:55 +0100

    Dashboard content index file has been modified

---
+++ src/pages/content/DashboardContent/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StudentDashboard from "./Student";
+import Officer from "./Officer";
+
+const DashboardContent= ()=>{
+    return(
+    <>
+    <Officer/>
+    <StudentDashboard/>
+    </>);
+}
+export default DashboardContent;
