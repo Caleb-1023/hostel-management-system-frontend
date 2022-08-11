bc58daeef8c922c177ad576759d76e6219cfdac9
sayoadesokan <sayosokan@gmail.com>
2022-08-11 12:31:16 +0100

    Resident Hall

--- src/components/ResidentHall/residentHall.jsx
+++ src/components/ResidentHall/residentHall.jsx
@@ -5,7 +5,36 @@ function residentHall() {
   return (
     <div className="resident">
       <div className="resident-body">
-        <div className="resident-container">residentHall</div>
+        <div className="resident-container">
+          <div className="title">Resident Halls</div>
+          <div className="image-controller">
+            <div className="resident-img">
+              <img
+                className="resident-image"
+                src="./images/Rectangle 43.svg"
+                alt=""
+              />
+              <img
+                className="resident-image"
+                src="./images/Rectangle 43.svg"
+                alt=""
+              />
+            </div>
+            <div className="resident-img">
+              <img
+                className="resident-image"
+                src="./images/Rectangle 43.svg"
+                alt=""
+              />
+              <img
+                className="resident-image"
+                src="./images/Rectangle 43.svg"
+                alt=""
+              />
+            </div>
+          </div>
+          <button className="btn">Back</button>
+        </div>
       </div>
     </div>
   );
