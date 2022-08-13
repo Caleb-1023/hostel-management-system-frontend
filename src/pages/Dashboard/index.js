ac3f4debb4a38ddb958a64ce3c0c25db9a01102b
Ese Okpongete <eseokpongs13@gmail.com>
2022-08-13 18:55:43 +0100

    ...

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
 // import Officer from '../content/DashboardContent/Officer';
-import StudentDashboard from '../content/DashboardContent/Student.js';
+//import StudentDashboard from '../content/DashboardContent/Student.js';
 import DashboardContent from '../content/DashboardContent';
 
 const Dashboard = () => {
